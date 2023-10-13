a827c3c9b424d170eaad52c0589c67d5773bcb83
yokinzou <146932419+yokinzou@users.noreply.github.com>
2023-10-13 08:43:40 +0800

    Update project-section.tsx

--- components/section/project-section.tsx
+++ components/section/project-section.tsx
@@ -27,76 +27,76 @@ async function getStarNumber(owner: string, repo: string) {
 }
 
 function ProjectTitle({ image, title, link }: ProjectTitleProps) {
-  return (
-    <Link
-      href={link}
-      target="_blank"
-      className="flex items-center gap-x-1 group"
-    >
-      <LabelWithGraphic image={image} content={title} />
-      <Icons.Link
-        size={12}
-        className="text-zinc-400 transition group-hover:text-zinc-700 dark:text-zinc-400 dark:group-hover:text-zinc-200 group-hover:animate-shake"
-      />
-    </Link>
-  );
+  // return (
+  //   // <Link
+  //   //   href={link}
+  //   //   target="_blank"
+  //   //   className="flex items-center gap-x-1 group"
+  //   // >
+  //   //   <LabelWithGraphic image={image} content={title} />
+  //   //   <Icons.Link
+  //   //     size={12}
+  //   //     className="text-zinc-400 transition group-hover:text-zinc-700 dark:text-zinc-400 dark:group-hover:text-zinc-200 group-hover:animate-shake"
+  //   //   />
+  //   // </Link>
+  // );
 }
 
 export default async function ProjectSection() {
   const exp: ExperienceProps[] = [
-    {
-      head1: (
-        <ProjectTitle
-          image="/images/logos/nextjs.png"
-          title="Next.js"
-          link="https://nextjs.org/"
-        />
-      ),
-      head2: (
-        <LabelWithGraphic icon={Icons.Stack} content="TypeScript, React, SSG" />
-      ),
-      head3: (
-        <LabelWithGraphic
-          icon={Icons.Star}
-          content={`# Github Stars: ${await getStarNumber(
-            "vercel",
-            "next.js"
-          )}`}
-        />
-      ),
-      head4: "Sep 2023 - Pres.",
-      bulletPoints: [
-        "Id ex et adipisicing proident excepteur.",
-        "Dolore ex commodo non et qui. Reprehenderit exercitation irure culpa sint nisi eiusmod amet ad occaecat quis.",
-        "Veniam esse enim adipisicing incididunt tempor minim irure occaecat cupidatat duis consectetur dolor.",
-        "Qui cupidatat adipisicing adipisicing aliqua irure id esse aute pariatur laborum non.",
-      ],
-    },
-    {
-      head1: (
-        <ProjectTitle
-          image="/images/logos/tailwind.webp"
-          title="Tailwind CSS"
-          link="https://tailwindcss.com/"
-        />
-      ),
-      head2: <LabelWithGraphic icon={Icons.Stack} content="HTML, CSS" />,
-      head3: <LabelWithGraphic icon={Icons.Star} content={`# Users: 412343`} />,
-      head4: "Sep 2023 - Pres.",
-      bulletPoints: [
-        "Supported user to create their resume with React, and easily export it as pdf (like this resume)",
-        "Enhanced resume with React, featuring responsive/interactive design, dark mode, showing real-time API data, and even integrating GPT etc.",
-      ],
-    },
+    // {
+    //   head1: (
+    //     <ProjectTitle
+    //       image="/images/logos/nextjs.png"
+    //       title="Next.js"
+    //       link="https://nextjs.org/"
+    //     />
+    //   ),
+    //   head2: (
+    //     <LabelWithGraphic icon={Icons.Stack} content="TypeScript, React, SSG" />
+    //   ),
+    //   head3: (
+    //     <LabelWithGraphic
+    //       icon={Icons.Star}
+    //       content={`# Github Stars: ${await getStarNumber(
+    //         "vercel",
+    //         "next.js"
+    //       )}`}
+    //     />
+    //   ),
+    //   head4: "Sep 2023 - Pres.",
+    //   bulletPoints: [
+    //     // "Id ex et adipisicing proident excepteur.",
+    //     // "Dolore ex commodo non et qui. Reprehenderit exercitation irure culpa sint nisi eiusmod amet ad occaecat quis.",
+    //     // "Veniam esse enim adipisicing incididunt tempor minim irure occaecat cupidatat duis consectetur dolor.",
+    //     // "Qui cupidatat adipisicing adipisicing aliqua irure id esse aute pariatur laborum non.",
+    //   ],
+    // },
+    // {
+    //   head1: (
+    //     <ProjectTitle
+    //       image="/images/logos/tailwind.webp"
+    //       title="Tailwind CSS"
+    //       link="https://tailwindcss.com/"
+    //     />
+    //   ),
+    //   head2: <LabelWithGraphic icon={Icons.Stack} content="HTML, CSS" />,
+    //   head3: <LabelWithGraphic icon={Icons.Star} content={`# Users: 412343`} />,
+    //   head4: "Sep 2023 - Pres.",
+    //   bulletPoints: [
+    //     // "Supported user to create their resume with React, and easily export it as pdf (like this resume)",
+    //     // "Enhanced resume with React, featuring responsive/interactive design, dark mode, showing real-time API data, and even integrating GPT etc.",
+    //   ],
+    // },
   ];
 
-  return (
-    <Section title="PROJECT">
-      <div className="flex flex-col gap-y-1">
-        {exp.map((e, index) => (
-          <Experience key={index} {...e} />
-        ))}
-      </div>
-    </Section>
+  return ( null
+    // <Section title="PROJECT">
+    //   {/* <div className="flex flex-col gap-y-1">
+    //     {exp.map((e, index) => (
+    //       <Experience key={index} {...e} />
+    //     ))}
+    //   </div> */}
+    //  </Section>
   );
 }
