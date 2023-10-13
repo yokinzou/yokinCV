6fa8e6aa956a1943be0d85ae3f51cac7460c19ed
yokinzou <146932419+yokinzou@users.noreply.github.com>
2023-10-13 08:47:59 +0800

    Update header-section.tsx

--- components/section/header-section.tsx
+++ components/section/header-section.tsx
@@ -13,15 +13,15 @@ export default function HeaderSection({ usage }: Props) {
     <section>
       <div className="flex justify-between items-center">
         <div className="flex items-center">
-          <h2 className="text-2xl font-bold mr-4 my-1">YOUR NAME</h2>
+          <h2 className="text-2xl font-bold mr-4 my-1">Yuanjian ZOU (Yokin)</h2>
           <SocialLink
-            href="https://github.com/vercel"
+            href="https://github.com/yokinzou"
             platform="github"
             aria-label="My GitHub"
             className="mx-1 h-4 w-4"
           />
           <SocialLink
-            href="https://www.linkedin.com/company/vercel"
+            href="https://www.linkedin.com/in/yuanjianzou/"
             platform="linkedin"
             aria-label="My LinkedIn"
             className="mx-1 h-4 w-4"
@@ -39,14 +39,14 @@ export default function HeaderSection({ usage }: Props) {
           className="group flex gap-2 items-center text-zinc-400 transition hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-200"
         >
           <Icons.Mail size={12} className="group-hover:animate-shake" />
-          your.name@email.com
+          yokinzou@outlook.com
         </Link>
         <Link
           href="tel:+11234567890"
           className="group flex gap-2 items-center text-zinc-400 transition hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-200"
         >
           <Icons.PhoneCall size={12} className="group-hover:animate-shake" />
-          (+1) 123-456-7890
+          (+86) 18820487196
         </Link>
       </div>
     </section>
