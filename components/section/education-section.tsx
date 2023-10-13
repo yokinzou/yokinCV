174e239db76b1fc573e6ad25be809045617cf87e
yokinzou <146932419+yokinzou@users.noreply.github.com>
2023-10-13 08:38:21 +0800

    Update education-section.tsx

--- components/section/education-section.tsx
+++ components/section/education-section.tsx
@@ -2,24 +2,27 @@ import Section from "./section";
 import { Icons } from "@/components/icons";
 import LabelWithGraphic from "@/components/label-with-graphic";
 
+
 const education: EducationProps[] = [
   {
-    title: "M.S. Business Analytics",
-   gpa: "Merit, Science Scholar",
+    title: "MSc. Business Analytics",
+    gpa: "Merit, Science Scholar",
     org: {
       name: "University of Liverpool",
       logo: "/images/logos/liverpool.jpg",
     },
-    time: "Sep 2020 - Mar 2022.",
+    time: "Sep 2020 - Mar 2022",
+
   },
   {
-    title: "BSc Accounting",
+    title: "BA. Accounting",
     gpa: "81.3/100",
     org: {
       name: "Guangxi University of Finance and Economics",
       logo: "/images/logos/GXUFE.jpg",
     },
-    time: "Jul 2019",
+    time: "Sep 2014 - Jun 2018",
+
   },
 ];
 
@@ -29,24 +32,51 @@ function Education({ title, gpa, org, time }: EducationProps) {
       <div className="hidden md:block">
         <div className="text-sm font-semibold flex flex-wrap justify-between text-zinc-700 dark:text-zinc-300">
           <div className="flex gap-x-4 flex-wrap">
+
+            < LabelWithGraphic image={org.logo} content={org.name} />
             <span className="font-bold text-black dark:text-white">
               {title}
             </span>
-            < LabelWithGraphic image={org.logo} content={org.name} />
-{/*             <LabelWithGraphic icon={Icons.FileBadge} content={gpa} /> */}
+            {/* <span>{title}</span> */}
           </div>
           <span>{time}</span>
+
+
+        {title==="MSc. Business Analytics" && (
+          <div className="md:flex text-sm">
+            <div className="w-32 flex-shrink-0 flex justify-between">
+              <span className="font-bold">Main Courses</span>:
+            </div>
+            <div className="font-normal">
+              Database and database managements, Data miming & machine learning, Data structures and Algorithm, Computer Network, Big data Application, Social network analysis, Econometrics
+            </div>
+          </div> )
+          } 
+
+          {title==="BA. Accounting" && (
+          <div className="md:flex text-sm">
+            <div className="w-32 flex-shrink-0 flex justify-between">
+              <span className="font-bold">Main Courses</span>:
+            </div>
+            <div className="font-normal">
+            Linear Algebra, Calculus, Probability Theory, Statistics, Advanced Financial Modeling, Finance, Economics, Advanced Accounting, Flash Animation Design
+            </div>
+          </div> )
+          }  
+
+
         </div>
-        <LabelWithGraphic image={org.logo} content={org.name} />
+        {/* {title} */}
       </div>
       <div className="text-sm font-semibold md:hidden text-zinc-700 dark:text-zinc-300 mb-1">
         <div className="flex gap-x-4 flex-wrap justify-between">
-          <div className="font-bold text-black dark:text-white">{title}</div>
+          <div className="font text-black dark:text-white">{title}</div>
           <LabelWithGraphic icon={Icons.FileBadge} content={gpa} />
         </div>
         <div className="flex gap-x-4 flex-wrap justify-between">
           <LabelWithGraphic image={org.logo} content={org.name} />
           <span className="text-right flex-1">{time}</span>
+          {/* <span className="text-right flex-1">{courses}</span> */}
         </div>
       </div>
     </section>
