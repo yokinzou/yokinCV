eca6c6c9746eb58d2ec6e2ea27a08ab0ddd17732
yokinzou <146932419+yokinzou@users.noreply.github.com>
2023-10-16 23:20:38 +0800

    Update work-section.tsx

--- components/section/work-section.tsx
+++ components/section/work-section.tsx
@@ -6,7 +6,7 @@ import { Icons } from "@/components/icons";
 const exp: ExperienceProps[] = [
   {
     head1: "Data Analytics Engineer",
-  ///  head2: <LabelWithGraphic icon={Icons.Stack} content="JavaScript, AWS" />,
+    ///  head2: <LabelWithGraphic icon={Icons.Stack} content="JavaScript, AWS" />,
     head3: (
       <LabelWithGraphic
         image="/images/logos/moomoo.jpg"
@@ -15,9 +15,8 @@ const exp: ExperienceProps[] = [
     ),
     head4: "Sep 2023 - Pres",
     bulletPoints: [
-      "Build the Power BI reports to monitor the business KPI.",
+      "Maintain the data warehouse and build the Power BI reports to monitor the business KPI.",
       "Build the machine learning pipeline to improve the new low quality user deposit ",
-      "Maintenance the warehouse by building the frequently used tables and views with hivesql"
     ],
   },
   {
@@ -31,18 +30,18 @@ const exp: ExperienceProps[] = [
     ),
     head4: "Feb 2022 - Sep 2023",
     bulletPoints: [
-      "Be responsible for building Shenzhen factory Power BI reports cooperating with cross-functional departments",
+      "I am the data engineer and data science technical lead in Shenzhen Factory and lead the PTM and other engineers to finish digital projects by following the sprint workflow",
+      "Building Power BI reports cooperating with cross-functional departments",
       "Build the ETL pipelines (Python,SSIS,Azure data factory,dbt,Kettle) to extract manufacturing BOM data to automate and optimize the business process",
       "Build the machine learning model to predict the actual BOM quantity to reduce  production overs",
       "Maintain data warehouse performance including tables and views redesign and query performance optimization",
       "Develop the automation algorithms(Python code) and applications (Power APP) to optimize company business process",
       "Provide data science training to company colleagues to discover valuable digital projects",
-      
     ],
   },
   {
     head1: "Data Scientist Intern",
-   // head2: <LabelWithGraphic icon={Icons.Stack} content="Content Creation" />,
+    // head2: <LabelWithGraphic icon={Icons.Stack} content="Content Creation" />,
     head3: (
       <LabelWithGraphic
         image="/images/logos/didi.jpg"
@@ -51,12 +50,10 @@ const exp: ExperienceProps[] = [
     ),
     head4: "Aug 2021 - Nov 2021",
     bulletPoints: [
-      "Lead the business data monitor to write the weekly analytics report for the business unit(Poupa car) of latin america ",
-      "Conduct AB Testing experiment to check the carpooling willingness among users to support the business decision",
-      "Build the Tableau dashboards to automate the data generation to help colleagues to get the data efficiently",
+      "Conduct AB Testing experiments to support the business decision",
+      "Build the Tableau dashboards to automate the data generation",
     ],
   },
-
 ];
 
 export default function WorkSection() {
