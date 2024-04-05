a3f04c17f7046e779071d03b140941208e545289
yokinzou <146932419+yokinzou@users.noreply.github.com>
2024-04-05 21:45:41 +0800

    Update education-section.tsx

--- components/section/education-section.tsx
+++ components/section/education-section.tsx
@@ -22,11 +22,10 @@ const education: EducationProps[] = [
     time: "Sep 2014 - Jun 2018",
   },
 ];
-
 function Education({ title, gpa, org, time }: EducationProps) {
   return (
     <section className="text-sm font-semibold text-zinc-700 dark:text-zinc-300">
-      <div className="hidden md:block">
+      <div className=""> {/* Removed 'hidden md:block' */}
         <div className="text-sm font-semibold flex flex-wrap justify-between text-zinc-700 dark:text-zinc-300">
           <div className="flex gap-x-4 flex-wrap">
             <LabelWithGraphic image={org.logo} content={org.name} />
@@ -38,20 +37,20 @@ function Education({ title, gpa, org, time }: EducationProps) {
           <span>{time}</span>
 
           {title === "MSc. Business Analytics" && (
-            <div className="md:flex text-sm">
+            <div className="flex text-sm">
               <div className="w-32 flex-shrink-0 flex justify-between">
                 <span className="font-bold">Main Courses</span>:
               </div>
               <div className="font-normal">
                 Database and database managements, Data miming & machine
-                learning, Data structures and Algorithm, Computer Network, Big
-                data Application, Social network analysis, Econometrics
+                learning, Data structures and algorithm, Computer network, Big
+                data application, Social network analysis, Econometrics
               </div>
             </div>
           )}
 
-          {/* {title === "BA. Accounting" && (
-            <div className="md:flex text-sm">
+          {title === "BA. Accounting" && (
+            <div className="flex text-sm">
               <div className="w-32 flex-shrink-0 flex justify-between">
                 <span className="font-bold">Main Courses</span>:
               </div>
@@ -61,24 +60,14 @@ function Education({ title, gpa, org, time }: EducationProps) {
                 Accounting, Flash Animation Design
               </div>
             </div>
-          )} */}
-        </div>
-        {/* {title} */}
-      </div>
-      <div className="text-sm font-semibold md:hidden text-zinc-700 dark:text-zinc-300 mb-1">
-        <div className="flex gap-x-4 flex-wrap justify-between">
-          <div className="font text-black dark:text-white">{title}</div>
-          <LabelWithGraphic icon={Icons.FileBadge} content={gpa} />
-        </div>
-        <div className="flex gap-x-4 flex-wrap justify-between">
-          <LabelWithGraphic image={org.logo} content={org.name} />
-          <span className="text-right flex-1">{time}</span>
-          {/* <span className="text-right flex-1">{courses}</span> */}
+          )}
         </div>
       </div>
+      {/* ... rest of the code remains the same */}
     </section>
   );
 }
+
 
 export default function EducationSection() {
   return (
